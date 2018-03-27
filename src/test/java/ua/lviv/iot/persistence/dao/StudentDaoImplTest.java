a0ba91e897f8464c44619f1bc02cb326ac7b1e4c
Zenoviy Veres <viruslviv@gmail.com>
2018-03-27 22:33:18 +0300

    format code to fit into screen during lecture

--- src/test/java/ua/lviv/iot/persistence/dao/StudentDaoImplTest.java
+++ src/test/java/ua/lviv/iot/persistence/dao/StudentDaoImplTest.java
@@ -11,9 +11,14 @@ class StudentDaoImplTest {
 
     @Test
     public void testManager() {
-        EntityManager em2 = Persistence.createEntityManagerFactory("test_manager").createEntityManager();
+
+        EntityManager em2 = Persistence.
+                createEntityManagerFactory("test_manager").
+                createEntityManager();
         StudentDaoImpl daoImpl = new StudentDaoImpl();
         daoImpl.setEntityManager(em2);
+        
+        // magic starts here
         daoImpl.persist(new Student("vassa", "masa"));
         daoImpl.findById(1);
         daoImpl.findById(2);
